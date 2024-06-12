0f3c10206e53bb7a2c261e7b836eca0b665979f6
JuanPabloDiaz <juanchodis@hotmail.com>
2024-06-12 08:31:35 -0400

    updating setting from default repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
+import tailwind from '@astrojs/tailwind'
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: "server",
-  adapter: cloudflare({ mode: "advanced" }),
+  output: 'server',
+  adapter: cloudflare({ mode: 'advanced' }),
   vite: {
     define: {
-      "process.env.TMDB_API_KEY": JSON.stringify(process.env.TMDB_API_KEY),
+      'process.env.TMDB_API_KEY': JSON.stringify(process.env.TMDB_API_KEY),
     },
   },
-});
+})
