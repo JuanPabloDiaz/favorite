e32cf879fe39edf599a7fed9b2952bd0277b3682
JuanPabloDiaz <juanchodis@hotmail.com>
2024-06-11 23:22:08 -0400

    uninstall vercel and install cloudflare back for astro deployment chore: Update npm dependencies for Astro and Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
+
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: vercel(),
+  adapter: cloudflare({ mode: "advanced" }),
   vite: {
     define: {
       "process.env.TMDB_API_KEY": JSON.stringify(process.env.TMDB_API_KEY),
