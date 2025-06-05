c6bb29513bf610532438b9dd8120e6f2ab102865
Juan Pablo Diaz <juan.diaz93@hotmail.com>
2025-06-05 09:49:07 -0400

    Add clean script to package.json and update dependencies
    - Added a "clean" script to remove build artifacts and reinstall dependencies.
    - Removed "@astrojs/cloudflare" from devDependencies.
    - Added "@astrojs/vercel" to dependencies.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,33 +1,33 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel';
+import sitemap from '@astrojs/sitemap';
 
-import tailwind from "@astrojs/tailwind";
-import cloudflare from "@astrojs/cloudflare";
-
-import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap({
-    filter: (page) => !page.includes('/api/'),
-    changefreq: 'weekly',
-    priority: 0.7,
-    lastmod: new Date(),
-  })],
-  site: "https://fav.jpdiaz.dev",
-  output: "server",
-  adapter: cloudflare({ mode: "advanced" }),
+  site: 'https://fav.jpdiaz.dev',
+  output: 'server', // Puedes cambiar a "static" si no usas SSR
+  adapter: vercel(),
+  integrations: [
+    tailwind(),
+    sitemap({
+      filter: (page) => !page.includes('/api/'),
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
+  ],
   vite: {
     define: {
-      "process.env.TMDB_API_KEY": JSON.stringify(process.env.TMDB_API_KEY),
-      "process.env.LISTEN_NOTES_API_KEY": JSON.stringify(process.env.LISTEN_NOTES_API_KEY)
+      'process.env.TMDB_API_KEY': JSON.stringify(process.env.TMDB_API_KEY),
+      'process.env.LISTEN_NOTES_API_KEY': JSON.stringify(process.env.LISTEN_NOTES_API_KEY),
     },
     resolve: {
       alias: {
         '@services': '/src/services',
         '@data': '/src/data',
         '@components': '/src/components',
-        '@layouts': '/src/layouts'
-      }
-    }
+        '@layouts': '/src/layouts',
+      },
+    },
   },
 });
